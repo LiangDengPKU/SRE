d0bc092dcc7ceb0860168712e1a03effce81a9e7
LiangDengPKU <45376868+LiangDengPKU@users.noreply.github.com>
2025-09-17 17:45:00 +0800

    Update CSPdarknet.py

--- nets/CSPdarknet.py
+++ nets/CSPdarknet.py
@@ -18,9 +18,7 @@ class Focus(nn.Module):
         self.conv = Conv(c1 * 4, c2, k, s, p, g, act)
 
     def forward(self, x):
-        # 320, 320, 12 => 320, 320, 64
         return self.conv(
-            # 640, 640, 3 => 320, 320, 12
             torch.cat(
                 [
                     x[..., ::2, ::2], 
@@ -37,36 +35,29 @@ class Conv(nn.Module):
         self.conv   = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
         self.bn     = nn.BatchNorm2d(c2, eps=0.001, momentum=0.03)
         self.act    = SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
-
     def forward(self, x):
         return self.act(self.bn(self.conv(x)))
-
     def fuseforward(self, x):
         return self.act(self.conv(x))
 
 class Bottleneck(nn.Module):
-    # Standard bottleneck
     def __init__(self, c1, c2, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, shortcut, groups, expansion
         super(Bottleneck, self).__init__()
         c_ = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c_, c2, 3, 1, g=g)
         self.add = shortcut and c1 == c2
-
     def forward(self, x):
         return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
 
 class C3(nn.Module):
-    # CSP Bottleneck with 3 convolutions
     def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
         super(C3, self).__init__()
-        c_ = int(c2 * e)  # hidden channels
+        c_ = int(c2 * e)  
         self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c1, c_, 1, 1)
-        self.cv3 = Conv(2 * c_, c2, 1)  # act=FReLU(c2)
+        self.cv3 = Conv(2 * c_, c2, 1)  
         self.m = nn.Sequential(*[Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)])
-        # self.m = nn.Sequential(*[CrossConv(c_, c_, 3, 1, g, 1.0, shortcut) for _ in range(n)])
-
     def forward(self, x):
         return self.cv3(torch.cat(
             (
@@ -76,14 +67,12 @@ class C3(nn.Module):
             , dim=1))
 
 class SPP(nn.Module):
-    # Spatial pyramid pooling layer used in YOLOv3-SPP
     def __init__(self, c1, c2, k=(5, 9, 13)):
         super(SPP, self).__init__()
         c_ = c1 // 2  # hidden channels
         self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c_ * (len(k) + 1), c2, 1, 1)
         self.m = nn.ModuleList([nn.MaxPool2d(kernel_size=x, stride=1, padding=x // 2) for x in k])
-
     def forward(self, x):
         x = self.cv1(x)
         return self.cv2(torch.cat([x] + [m(x) for m in self.m], 1))
@@ -91,51 +80,22 @@ class SPP(nn.Module):
 class CSPDarknet(nn.Module):
     def __init__(self, base_channels, base_depth, phi, pretrained):
         super().__init__()
-        #-----------------------------------------------#
-        #   input: 640, 640, 3
-        #   base_channels: 64
-        #-----------------------------------------------#
-
-        #-----------------------------------------------#
-        #   640, 640, 3 -> 320, 320, 12 -> 320, 320, 64
-        #-----------------------------------------------#
         self.stem       = Focus(3, base_channels, k=3)
-        
-        #-----------------------------------------------#
-        #   conv      320, 320, 64 -> 160, 160, 128
-        #   CSPlayer  160, 160, 128 -> 160, 160, 128
-        #-----------------------------------------------#
         self.dark2 = nn.Sequential(
-            # 320, 320, 64 -> 160, 160, 128
             Conv(base_channels, base_channels * 2, 3, 2),
-            # 160, 160, 128 -> 160, 160, 128
             C3(base_channels * 2, base_channels * 2, base_depth),
         )
         
-        #-----------------------------------------------#
-        #   conv  160, 160, 128 -> 80, 80, 256
-        #   CSPlayer  80, 80, 256 -> 80, 80, 256
-
-        #-----------------------------------------------#
         self.dark3 = nn.Sequential(
             Conv(base_channels * 2, base_channels * 4, 3, 2),
             C3(base_channels * 4, base_channels * 4, base_depth * 3),
         )
 
-        #-----------------------------------------------#
-        #   conv      80, 80, 256 -> 40, 40, 512
-        #   CSPlayer  40, 40, 512 -> 40, 40, 512
-        #-----------------------------------------------#
         self.dark4 = nn.Sequential(
             Conv(base_channels * 4, base_channels * 8, 3, 2),
             C3(base_channels * 8, base_channels * 8, base_depth * 3),
         )
         
-        #-----------------------------------------------#
-        #   conv      40, 40, 512 -> 20, 20, 1024
-        #   SPP       20, 20, 1024 -> 20, 20, 1024
-        #   CSPlayer  20, 20, 1024 -> 20, 20, 1024
-        #-----------------------------------------------#
         self.dark5 = nn.Sequential(
             Conv(base_channels * 8, base_channels * 16, 3, 2),
             SPP(base_channels * 16, base_channels * 16),
@@ -143,31 +103,22 @@ class CSPDarknet(nn.Module):
         )
         if pretrained:
             pth_file = {
-                's' : './pretrained/cspdarknet_s_backbone.pth',
-                'm' : './pretrained/cspdarknet_m_backbone.pth',
-                'l' : './pretrained/cspdarknet_l_backbone.pth',
-                'x' : './pretrained/cspdarknet_x_backbone.pth',
+                's' : '/media/mediway/Work2/deep3/SRE-master/pretrained/cspdarknet_s_backbone.pth',
+                'm' : '/media/mediway/Work2/deep3/SRE-master/pretrained/cspdarknet_m_backbone.pth',
+                'l' : '/media/mediway/Work2/deep3/SRE-master/pretrained/cspdarknet_l_backbone.pth',
+                'x' : '/media/mediway/Work2/deep3/SRE-master/pretrained/cspdarknet_x_backbone.pth',
             }[phi]
             checkpoint = torch.load(pth_file)
             self.load_state_dict(checkpoint, strict=False)
-            print("Load weights")
-            
+            print("Load weights")            
+
     def forward(self, x):
         x = self.stem(x)
         x = self.dark2(x)
-        #-----------------------------------------------#
-        #   dark3  80, 80, 256 
-        #-----------------------------------------------#
         x = self.dark3(x)
         feat1 = x
-        #-----------------------------------------------#
-        #   dark4  40, 40, 512 
-        #-----------------------------------------------#
         x = self.dark4(x)
         feat2 = x
-        #-----------------------------------------------#
-        #   dark5  20, 20, 1024
-        #-----------------------------------------------#
         x = self.dark5(x)
         feat3 = x
         return feat1, feat2, feat3
